50f8f0d2e1078f02dcad6eb72cb789069d1c54a9
Jeremy Green <jeremy@octolabs.com>
2014-05-03 14:02:43 -0500

    Initial project generation with ember-cli

---
+++ app/app.js
@@ -0,0 +1,13 @@
+import Resolver from 'ember/resolver';
+import loadInitializers from 'ember/load-initializers';
+
+Ember.MODEL_FACTORY_INJECTIONS = true;
+
+var App = Ember.Application.extend({
+  modulePrefix: 'canary-ember', // TODO: loaded via config
+  Resolver: Resolver
+});
+
+loadInitializers(App, 'canary-ember');
+
+export default App;
