2fd8af3e966a7a53ac1e15b367416c2632054ca5
Jeremy Green <jeremy@octolabs.com>
2014-05-05 01:01:19 -0500

    Slightly fewer measurements per request

--- app/adapters/measurement.js
+++ app/adapters/measurement.js
@@ -3,7 +3,7 @@ export default DS.RESTAdapter.extend({
   namespace : 'checks',
   findQuery: function(store, type, query) {
     var url = this.buildURL('');
-    url += "/" + query.check_id + "/measurements" + "?range=180";
+    url += "/" + query.check_id + "/measurements" + "?range=150";
     return this.ajax(url, 'GET', { });
   }
 });
