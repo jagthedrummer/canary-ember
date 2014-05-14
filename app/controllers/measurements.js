2e024bf78c6940ca4adccc794538a19ab8b35a33
Jeremy Green <jeremy@octolabs.com>
2014-05-14 00:05:09 -0500

    Remove some of the original code

--- app/controllers/measurements.js
+++ app/controllers/measurements.js
@@ -36,23 +36,6 @@ var MeasurementsController = Ember.ArrayController.extend({
     this.set('currentHttpPieData',locationData.http_status);
     this.set('currentLocationPieData',locationData.location);
     
-    /*
-    if( this.get('isOverview') ){
-      this.set('currentLineData',this.buildOverviewLineData());
-      this.set('currentPrimaryIpPieData',this.filterPieChartData('content','primary_ip'));
-      this.set('currentLocalIpPieData',this.filterPieChartData('content','local_ip'));
-      this.set('currentExitPieData',this.filterPieChartData('content','exit_status'));
-      this.set('currentHttpPieData',this.filterPieChartData('content','http_status'));
-      this.set('currentLocationPieData',this.filterPieChartData('content','location'));
-    }else{
-      var location = this.get('currentLocation').replace('-','');
-      this.set('currentLineData',this.buildLocationLineData(location));
-      this.set('currentPrimaryIpPieData',this.filterPieChartData(location,'primary_ip'));
-      this.set('currentLocalIpPieData',this.filterPieChartData(location,'local_ip'));
-      this.set('currentExitPieData',this.filterPieChartData(location,'exit_status'));
-      this.set('currentHttpPieData',this.filterPieChartData(location,'http_status'));
-    }
-   */
   }.observes('masterLocationData','currentLocation'),
 
   // Make one pass through the batch and extract various things
@@ -76,17 +59,16 @@ var MeasurementsController = Ember.ArrayController.extend({
         locationData[attName].values.unshift({x:t,y:item.get(attName)});
       });
 
-      
       pieAttNames.forEach(function(attName){
         _this.handlePieCount(allData,attName,item);
         _this.handlePieCount(locationData,attName,item);
       });
-      
+
     });
 
     var keys = Object.keys(locations).sort();
     var allCombinedData = [];
-    keys.forEach(function(locationName) { 
+    keys.forEach(function(locationName) {
       var locationData = locations[locationName];
       locationData.combinedTiming = [
         locationData.total_time,
@@ -110,9 +92,9 @@ var MeasurementsController = Ember.ArrayController.extend({
         });
         locationData[attName] = data;
       });
-    });
+    }); // keys.forEach
     locations.all.combinedTiming = allCombinedData;
-
+    console.log('masterLocationData = ', locations);
     this.set('masterLocationData',locations);
   }.observes('model'),
 
@@ -152,34 +134,7 @@ var MeasurementsController = Ember.ArrayController.extend({
     };
   },
 
-  filterLineChartData : function(source,dataAtt,title){
-    var values = this.get(source).map(function(item){
-      var t = item.get('t');
-      return {
-        x : t,
-        y : item.get(dataAtt),
-        key : item.get('id')
-      }
-    });
-    return [{key : title, values : values}];
-
-  },
-
-  filterPieChartData : function(source,dataAtt){
-    var data = [];
-    var map = {};
-    this.get(source).forEach(function(item){
-      var info = map[item.get(dataAtt)];
-      if(!info){
-        info = {"label":item.get(dataAtt),value:0};
-        map[item.get(dataAtt)] = info;
-        data.push(info);
-      }
-      info.value += 1;
-    });
-    return data;
-  },
-
+  
   totalTimeData : function(){
   },
 
@@ -195,100 +150,6 @@ var MeasurementsController = Ember.ArrayController.extend({
     return this.get('locations').uniq().sort();
   }.property('model'),
 
-  doams2 : function(){
-    return this.get('arrangedContent').filterBy('location','do-ams2');
-  }.property('model'),
-
-  dosf1 : function(){
-    return this.get('arrangedContent').filterBy('location','do-sf1');
-  }.property('model'),
-
-  dony2 : function(){
-    return this.get('arrangedContent').filterBy('location','do-ny2');
-  }.property('model'),
-
-  dosin1 : function(){
-    return this.get('arrangedContent').filterBy('location','do-sin1');
-  }.property('model'),
-
-  setDoams2LineData : function(){
-    this.setLocationLineData('doams2','doams2LineData');
-    
-    this.setLocationPieData('doams2','exit_status','doams2ExitPieData');
-    this.setLocationPieData('doams2','http_status','doams2HttpPieData');
-    this.setLocationPieData('doams2','local_ip','doams2LocalIpPieData');
-    this.setLocationPieData('doams2','primary_ip','doams2PrimaryIpPieData');
-  }.observes('model'),
-
-  setDosf1LineData : function(){
-    this.setLocationLineData('dosf1','dosf1LineData');
-  }.observes('model'),
-
-  setDony2LineData : function(){
-    this.setLocationLineData('dony2','dony2LineData');
-  }.observes('model'),
-
-  setDosin1LineData : function(){
-    this.setLocationLineData('dosin1','dosin1LineData');
-  }.observes('model'),
-
-  setLocationPieData : function(location,sourceAtt,targetAtt){
-    if( this.get('content.length') == 0 ){
-      return;
-    }
-    var data = this.filterPieChartData(location,sourceAtt);
-    this.set(targetAtt,data);
-  },
-
-  buildLocationLineData : function(location){
-    var data = [];
-    data.push(this.filterLineChartData(location,'total_time','Total')[0]);
-    data.push(this.filterLineChartData(location,'starttransfer_time','Start Transfer')[0]);
-    data.push(this.filterLineChartData(location,'connect_time','Connect')[0]);
-    data.push(this.filterLineChartData(location,'namelookup_time','Name Lookup')[0]);
-    return data;
-  },
-
-
-  setLocationLineData : function(location,targetAtt){
-    if( this.get('content.length') == 0 ){
-      return;
-    }
-    this.set(targetAtt,this.buildLocationLineData(location));
-  },
-
-  buildOverviewLineData : function(){
-    var data = [];
-    data.push(this.filterLineChartData('doams2','total_time','do-ams2')[0]);
-    //data.push(this.filterLineChartData('doams2','starttransfer_time','Start Transfer Time : do-ams2')[0]);
-    //data.push(this.filterLineChartData('doams2','connect_time','Connect Time : do-ams2')[0]);
-    //data.push(this.filterLineChartData('doams2','namelookup_time','Name Lookup Time : do-ams2')[0]);
-    
-    data.push(this.filterLineChartData('dosf1','total_time','do-sf1')[0]);
-    //data.push(this.filterLineChartData('dosf1','starttransfer_time','Start Transfer Time : do-sf1')[0]);
-    //data.push(this.filterLineChartData('dosf1','connect_time','Connect Time : do-sf1')[0]);
-    //data.push(this.filterLineChartData('dosf1','namelookup_time','Name Lookup Time : do-sf1')[0]);
-    
-    data.push(this.filterLineChartData('dony2','total_time','do-ny2')[0]);
-    //data.push(this.filterLineChartData('dony2','starttransfer_time','Start Transfer Time : do-ny2')[0]);
-    //data.push(this.filterLineChartData('dony2','connect_time','Connect Time : do-ny2')[0]);
-    //data.push(this.filterLineChartData('dony2','namelookup_time','Name Lookup Time : do-ny2')[0]);
-    
-    data.push(this.filterLineChartData('dosin1','total_time','do-sin1')[0]);
-    //data.push(this.filterLineChartData('dosin1','starttransfer_time','Start Transfer Time : do-sin1')[0]);
-    //data.push(this.filterLineChartData('dosin1','connect_time','Connect Time : do-sin1')[0]);
-    //data.push(this.filterLineChartData('dosin1','namelookup_time','Name Lookup Time : do-sin1')[0]);
-    
-    return data;
-  },
-
-  setAllTheData : function(){
-    if( this.get('content.length') == 0 ){
-      return;
-    }
-    
-    this.set('allTheData',this.buildOverviewLineData());
-  }.observes('model'),
 
   updateContent : function(){
     var _this = this;
