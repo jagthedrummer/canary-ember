95d651da112387b8da00980835267948f885c013
Jeremy Green <jeremy@octolabs.com>
2014-05-13 23:16:32 -0500

    Go back to hash location.

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 module.exports = function(environment) {
   var ENV = {
     baseURL: '/',
-    locationType: 'auto',
+    locationType: 'hash', //'auto',
     FEATURES: {
       // Here you can enable experimental features on an ember canary build
       // e.g. 'with-controller': true
