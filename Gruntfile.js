1b30ef63e336d53236ee73c5205641510efda8a3
Jeremy Green <jeremy@octolabs.com>
2014-05-23 22:08:37 -0500

    Add asset fingerprinting and reduce cache time for r index

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,15 @@ module.exports = function(grunt) {
             src: 'dist/*',
 
             // But if you use wildcards, make sure your destination is a directory.
-            dest: ''
+            dest: '',
+
+            options: {
+              headers: {
+                // 1 minute cache policy (1000 * 60)
+                "Cache-Control": "max-age=60000, public",
+                "Expires": new Date(Date.now() + 60000).toUTCString()
+              }
+            }
           },
           {
             // Wildcards are valid *for uploads only* until I figure out a good implementation
@@ -44,12 +52,24 @@ module.exports = function(grunt) {
 
       }
 
-    }
+    }, // end s3
+
+    hashres: {
+      prod: {
+        src: [
+          'dist/assets/app.js',
+          'dist/assets/vendor.css',
+          'dist/assets/app.css'
+        ],
+        dest: 'dist/index.html',
+      }
+    } // end hashres
   });
 
   grunt.loadNpmTasks('grunt-s3');
+  grunt.loadNpmTasks('grunt-hashres');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['hashres','s3']);
 
 };
