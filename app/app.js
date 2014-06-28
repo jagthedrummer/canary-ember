9c39d5d3bf417ce9080ddcdb4ba72f7c7430f5cf
Rick Yoesting <rick.yoesting@gmail.com>
2014-06-28 16:27:04 -0500

    add config app config flag USE_CUSTOM_HOSTS and place the canary-hosts.json check behind it.

--- app/app.js
+++ app/app.js
@@ -1,20 +1,22 @@
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 
+if (ENV.APP.USE_CUSTOM_HOSTS) {
+	$.get('canary-hosts.json').done(function(data){
+	  //Ember.Logger.log(data);
+	  ENV.APP.CANARY_API_HOST = data.api;
+	  ENV.APP.CANARY_CHECKS_HOST = data.checks;
+	}).fail(function(){
+	  //we ain't got one!
+	  Ember.Logger.log('use default canary hosts!');
+	});
+}
+
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
   modulePrefix: 'canary-ember', // TODO: loaded via config
   Resolver: Resolver
-});
-
-$.get('canary-hosts.json').done(function(data){
-  //Ember.Logger.log(data);
-  ENV.APP.CANARY_API_HOST = data.api;
-  ENV.APP.CANARY_CHECKS_HOST = data.checks;
-}).fail(function(){
-  //we ain't got one!
-  Ember.Logger.log('use default canary hosts!');
 });
 
 loadInitializers(App, 'canary-ember');
